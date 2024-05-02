fddbc9347f9ab60d9093311cb04a232291658c01
himanshuparmar21 <himanshu.parmar21204@gmail.com>
2024-05-03 00:41:15 +0530

    Add Cyclic Sort

--- src/Lecture_11_Cyclick_Sort/Q6_First_Missing_Position.java
+++ src/Lecture_11_Cyclick_Sort/Q6_First_Missing_Position.java
@@ -11,17 +11,17 @@ public class Q6_First_Missing_Position {
         cyclicSort(nums);
 
         for(int i=1;i<nums.length;i++){
-            if(i != nums[i]){
-                return i;
+            if(i+1 != nums[i]){
+                return i+1;
             }
         }
-        return nums.length;
+        return nums.length+1;
     }
     static void cyclicSort(int[] arr){
         int i = 0;
         while(i < arr.length){
-            int current = arr[i];
-            if(current > -1 && arr[i] != arr[current]){
+            int current = arr[i] - 1;
+            if(arr[i] > -1 && arr[i] <= arr.length && arr[i] != arr[current]){
                 int temp = arr[i];
                 arr[i] = arr[current];
                 arr[current] = temp;
