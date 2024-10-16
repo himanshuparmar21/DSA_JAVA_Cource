9fd654fb426045287fde9c9cd5714955d4615c72
himanshuparmar21 <himanshu.parmar21204@gmail.com>
2024-10-16 19:33:06 +0530

    Add Problem

---
+++ src/Problems/LongestHappyString.java
@@ -0,0 +1,88 @@
+package Problems;
+
+import java.util.PriorityQueue;
+// https://leetcode.com/problems/longest-happy-string/
+public class LongestHappyString {
+    public static void main(String[] args) {
+        System.out.println(longestDiverseString(1,1,7));
+    }
+    public static String longestDiverseString(int a, int b, int c) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        if(a>0){
+            pq.offer(new Pair(a, 'a'));
+        }
+        if(b>0){
+            pq.offer(new Pair(b, 'b'));
+        }
+        if(c>0){
+            pq.offer(new Pair(c, 'c'));
+        }
+
+        StringBuilder res = new StringBuilder();
+        while(!pq.isEmpty()){
+            Pair node = pq.poll();
+            int n = res.length();
+            char ch = node.ch;
+            int count = node.count;
+            if(n>=2 && res.charAt(n-1) == ch && res.charAt(n-2) == ch) {
+                if(pq.isEmpty()) break;
+                Pair sec = pq.poll();
+                res.append(sec.ch);
+                sec.count--;
+                if(sec.count>0){
+                    pq.offer(new Pair(sec.count, sec.ch));
+                }
+            }else{
+                res.append(node.ch);
+                node.count--;
+            }
+            // if element count is not 0, insert in pq with updated count
+            if(node.count>0){
+                pq.offer(new Pair(node.count, node.ch));
+            }
+        }
+        return res.toString();
+
+        //Second Way
+//        StringBuilder sb = new StringBuilder();
+//
+//        int totalLength = a + b + c;
+//        int continuousA = 0, continuousB = 0, continuousC = 0;
+//
+//        for(int i = 0; i < totalLength; i++) {
+//            if ((a >= b && a >= c && continuousA != 2) || (continuousB == 2 && a > 0) || (continuousC == 2 && a > 0))  {
+//                sb.append("a");
+//                a--;
+//                continuousA++;
+//                continuousB = 0;
+//                continuousC = 0;
+//            } else if ((b >= a && b >= c && continuousB != 2) || (continuousA == 2 && b > 0) || (continuousC == 2 && b > 0)) {
+//                sb.append("b");
+//                b--;
+//                continuousB++;
+//                continuousA = 0;
+//                continuousC = 0;
+//            } else if ((c >= a && c >= b && continuousC != 2) || (continuousB == 2 && c > 0) || (continuousA == 2 && c > 0)) {
+//                sb.append("c");
+//                c--;
+//                continuousC++;
+//                continuousA = 0;
+//                continuousB = 0;
+//            }
+//        }
+//        return sb.toString();
+    }
+
+
+static class Pair implements Comparable<Pair> {
+    int count;
+    char ch;
+    Pair(int count, char ch){
+        this.count = count;
+        this.ch = ch;
+    }
+    public int compareTo(Pair that){
+        return that.count - this.count;
+    }
+}
+}
---
+++ src/Problems/SubarrayrangewithgivensumGFG.java
@@ -0,0 +1,16 @@
+package Problems;
+
+public class SubarrayrangewithgivensumGFG {
+    public static void main(String[] args) {
+        System.out.println(subArraySum(new int[]{10, 2, -2, -20, 10},-10));
+    }
+    static int subArraySum(int arr[], int tar) {
+        int sum = 0;
+        int ans = 0;
+
+        int i = 0;
+        int start = 0;
+
+        return 1;
+    }
+}
